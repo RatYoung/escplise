68feac6f4f505166ed35e718fcfe44325ce66991
RatYoung <yhrnju@qq.com>
2017-10-21 17:39:42 +0800

    add

--- src/helloworld.java
+++ src/helloworld.java
@@ -4,7 +4,7 @@ public class helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello, world");
-		//System.out.println("杨浩然超帅");
+		System.out.println("杨浩然超帅");
 	}
 
 }
